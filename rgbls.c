6e951a02017c6f4981260781b55e94727bd58273
Demo User <debian@beaglebone.localdomain>
2016-06-16 00:36:08 +0000

    added adc sampling support

--- rgbls.c
+++ rgbls.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
-//#include <pthread.h>
-//#include <semaphore.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
 // #include <rt.h> // is this a library?? will probably need this for the linker
 
 //#include "fftlib/fftlib.h"
@@ -13,7 +15,7 @@
 #define	STRIP_LENGTH	60
 #define	NUM_STRIPS	4
 #define	MATRIX_STRIP_LENGTH	256
-#define	PORT	7890
+#define	PORT	5568
 #define	SERVER_ADDRESS	"::1"
 
 // double-buffer data members
@@ -25,7 +27,31 @@ rgb_strip strips[NUM_STRIPS];
 rgb_strip matrix;
 
 // control flow
-//sem_t timer_sem;
+sem_t timer_sem;
+
+/*
+* int adc(unsigned int chan)
+* This function was written with help from Robert Edwards' blog.
+* Available: embedded-basics.blogspot.com/2014/10/beaglebone-black-analog-input-pins.html
+*/
+
+int adc(unsigned int chan)
+{
+	int fd;
+	char buff[40];
+	char value[4];
+	snprintf(buff, sizeof(buff), "/sys/devices/ocp.2/helper.14/AIN%d", chan);
+	fd = open(buff, O_RDONLY);
+	if (fd < 0)
+	{
+		printf("adc open failed \n");
+		exit(1);
+	}
+	read(fd, &value, 4);
+	close(fd);
+	return atoi(value);
+	return -1;
+}
 
 int main(void)
 {
@@ -38,15 +64,16 @@ int main(void)
 	// initialize FFT singleton
 	// int ret;
 	// ret = fftlib_init();
+
 	// set up network connection with OPS server
 	if(opc_client_init(PORT, SERVER_ADDRESS))
 	{
 		exit(1);
 	}
-	
+	// for debugging
 	for (int i = 0; i < 30; ++i)
 	{
-		if (opc_client_send_formatted(0,0,&strips[0]));
+		if (opc_client_send_formatted(1,0,&strips[0]));
 	}
 	// initialize I/O for buttons
 	// initialize state in free-mode
