153de9c809fd1e64b41765bef3fbe13173282d65
herronee <emily.e.herron@vanderbilt.edu>
2017-12-01 00:23:04 -0600

    Add files via upload

---
+++ gpio.h
@@ -0,0 +1,24 @@
+//
+//  gpio.h
+//  
+//
+//  Created by emherron on 11/30/17.
+//
+//
+
+#ifndef gpio_h
+#define gpio_h
+
+#include <stdio.h>
+
+void gpio_initialize();
+
+int* checkLeftButton(void* arg);
+
+int* checkRightButton(void* arg);
+
+int* checkMode(void* arg);
+
+int* checkADC(void* arg);
+
+#endif /* gpio_h */
