63f561164a16ec76259876a3dcd794a10fa532b1
herronee <emily.e.herron@vanderbilt.edu>
2017-12-01 04:55:46 -0600

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -1,3 +1,11 @@
+//
+//  gpio.c
+//  
+//
+//  Created by emherron on 11/30/17.
+//
+//
+
 #include "gpio.h"
 #include "rgbls.h"
 #include "rgbls_game.h"
@@ -7,43 +15,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <gpio.h>
 
-#define LEFT_BUTTON_IN = 67;  //Linux ID number for GPIO you wish to use
-#define RIGHT_BUTTON_IN = 68;
-#define MODE_INPUT_IN = 44;
-#define ADC_INPUT_IN = 26;
+#include "libsoc_gpio.h"
+#include "libsoc_debug.h"
+
+
+#define LEFT_BUTTON_IN = 115;  //P927 //Linux ID number for GPIO you wish to use
+#define RIGHT_BUTTON_IN = 117;   //P925
 
 gpio *left_button;
 gpio *right_button;
-gpio *mode_input;
-gpio *adc_input;
 
 void gpio_initialize() {
 
     left_button = libsoc_gpio_request(LEFT_BUTTON_IN,LS_GPIO_SHARED);
     right_button = libsoc_gpio_request(RIGHT_BUTTON_IN,LS_GPIO_SHARED);
-    mode_input = libsoc_gpio_request(MODE_INPUT_IN,LS_GPIO_SHARED);
     
-    if (left_button==NULL || right_button==NULL || mode_input==NULL || adc_input==NULL) {
+    if (left_button==NULL || right_button==NULL) {
         printf("libsoc_gpio_request failed");
         exit();
     }
     
     libsoc_gpio_set_direction(left_button, INPUT);
     libsoc_gpio_set_direction(right_button, INPUT);
-    libsoc_gpio_set_direction(mode_input, INPUT);
     
     libsoc_gpio_set_level(left_button, HIGH);
     libsoc_gpio_set_level(right_button, HIGH);
-    libsoc_gpio_set_level(mode_input, HIGH);
     
     libsoc_gpio_set_edge(left_button, FALLING);
     libsoc_gpio_set_edge(right_button, FALLING);
-    libsoc_gpio_set_edge(mode_input, FALLING);
     
     libsoc_gpio_callback_interrupt(left_button, &checkLeftButton, (void*)NULL);
     libsoc_gpio_callback_interrupt(right_button, &checkRightButton, (void*)NULL);
-    libsoc_gpio_callback_interrupt(mode_input, &checkMode, (void*)NULL);
 
 }
 
@@ -55,13 +59,7 @@ int* checkRightButton(void* arg) {
     controlPlayer(2);
 }
 
-int* checkMode(void* arg) {
-    if (GAME_MODE == 1) {
-        GAME_MODE = 0;
-    } else {
-        GAME_MODE = 1;
-    }
-}
+
 
 
 
